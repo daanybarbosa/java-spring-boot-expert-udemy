78fe87f42fe752deccd12d758770935c3b36dfc1
Daniele Barbosa <daanybsilva@gmail.com>
2024-01-25 18:04:31 -0300

    hello world

--- vendas/src/main/java/estudos/VendasApplication.java
+++ vendas/src/main/java/estudos/VendasApplication.java
@@ -2,9 +2,17 @@ package estudos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication //Classe que irá inicializar o spring boot
+@RestController
 public class VendasApplication {
+
+    @GetMapping("/hello") //mapeando para o browser acessar essa url
+    public String helloWorld(){
+        return "hello world";
+    }
 
     public static void main(String[] args){
         SpringApplication.run(VendasApplication.class, args); //classe que irá inicar a aplicação / argumentos
