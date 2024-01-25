37f2c31d57fc8af2d479c0acff5c562f97fd36af
Daniele Barbosa <daanybsilva@gmail.com>
2024-01-25 18:39:40 -0300

    configurations e beans

--- vendas/src/main/java/estudos/VendasApplication.java
+++ vendas/src/main/java/estudos/VendasApplication.java
@@ -1,20 +1,40 @@
 package estudos;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication //Classe que irá inicializar o spring boot
+@SpringBootApplication
 @RestController
 public class VendasApplication {
 
-    @GetMapping("/hello") //mapeando para o browser acessar essa url
+    @Autowired //injestão
+    @Qualifier("applicationName")
+    private String applicationName;
+
+    @Autowired //injestão
+    @Qualifier("otherApplicationName")
+    private String otherApplicationName;
+
+    @GetMapping("/application")
+    public String application(){
+        return applicationName;
+    }
+
+    @GetMapping("/otherApplication")
+    public String otherApplication(){
+        return otherApplicationName;
+    }
+
+    @GetMapping("/hello")
     public String helloWorld(){
         return "hello world";
     }
 
     public static void main(String[] args){
-        SpringApplication.run(VendasApplication.class, args); //classe que irá inicar a aplicação / argumentos
+        SpringApplication.run(VendasApplication.class, args);
     }
 }
