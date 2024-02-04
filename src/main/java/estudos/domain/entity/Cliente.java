ad10daf16c3dae585322bbdf52536b0c4e5b46d5
Daniele Barbosa <daanybsilva@gmail.com>
2024-02-04 19:23:46 -0300

    criando as classes de modelo

---
+++ src/main/java/estudos/domain/entity/Cliente.java
@@ -0,0 +1,13 @@
+package estudos.domain.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Cliente {
+
+    private Integer id;
+    private String nome;
+
+}
---
+++ src/main/java/estudos/domain/entity/ItemPedido.java
@@ -0,0 +1,15 @@
+package estudos.domain.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ItemPedido {
+
+    private Integer id;
+    private Pedido pedido;
+    private Produto produto;
+    private Integer quantidade;
+
+}
