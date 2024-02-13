bb188a2ee2bbe32f0bdc158af9fa707f2de07cc5
Daniele Barbosa <daanybsilva@gmail.com>
2024-02-13 15:06:13 -0300

    mapeando a entidade Cliente para JPA

--- src/main/java/estudos/domain/entity/Cliente.java
+++ src/main/java/estudos/domain/entity/Cliente.java
@@ -4,12 +4,22 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
 @ToString
+@Entity //incluindo JPA
+//@Table(name = "tb_cliente", schema = "vendas") //incluindo JPA - exemplo - caso o nome da tabela no BD seja diferente da classe
+@Table( name = "cliente" ) //incluindo JPA
 public class Cliente {
 
+    @Id //defini o primary key dessa entidade
+    @GeneratedValue(strategy = GenerationType.AUTO) //implementar o auto increment no banco de dados
+    @Column(name = "id") //nome da coluna no banco de dados
     private Integer id;
+
+    @Column(name = "nome", length = 100)
     private String nome;
 
     public Cliente(){ };
