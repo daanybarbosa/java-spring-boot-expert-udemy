39175cc948b2a5aa0f28cd4d56a3660fe9d27514
Daniele Barbosa <daanybsilva@gmail.com>
2024-02-20 17:00:21 -0300

    trabalhando com @Query

--- src/main/java/estudos/domain/repositorio/Clientes.java
+++ src/main/java/estudos/domain/repositorio/Clientes.java
@@ -2,21 +2,10 @@ package estudos.domain.repositorio;
 
 import estudos.domain.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface Clientes extends JpaRepository<Cliente, Integer> {
-
-    //Query(value = " select c from Cliente c where c.nome like :nome ") //HQL (Hibernate Query Language)
-    @Query(value = " select * from cliente c where c.nome like '%:nome%' ", nativeQuery = true) //SQL Nativo
-    List<Cliente> encontrarPorNome(@Param("nome") String nome);
-
-    @Query(value = "delete from Cliente c where c.nome = :nome")
-    @Modifying //anotação para modificação de tabela
-    void deleteByNome(String nome);
 
     // select c from Cliente c where c.nome like :nome;
     List<Cliente> findByNomeLike(String nome); //query methods
