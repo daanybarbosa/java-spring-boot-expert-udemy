9a8ce92add12b45da24cd5cee0d00409d88027f1
Daniele Barbosa <daanybsilva@gmail.com>
2024-02-20 15:17:09 -0300

    query methods

--- src/main/java/estudos/VendasApplication.java
+++ src/main/java/estudos/VendasApplication.java
@@ -17,27 +17,33 @@ public class VendasApplication {
     public CommandLineRunner init(@Autowired Clientes clientes){
         return args -> {
 
-            System.out.println("Salvando clientes");
+            System.out.println("<--- Salvando clientes --->");
             clientes.save(new Cliente("Daniele"));
             clientes.save(new Cliente("Bruno"));
 
             List<Cliente> todosClientes = clientes.findAll();
             todosClientes.forEach(System.out::println); //imprimir todas os dados
 
-            System.out.println("Atualizando clientes");
+            System.out.println(" <--- Localizando clientes --->");
+            boolean existeDaniele = clientes.existsByNome("Daniele");
+            boolean existeCliente = clientes.existsByNome("Cliente");
+            System.out.println("Existe um cliente com o nome Daniele ? " + existeDaniele);
+            System.out.println("Existe um cliente com o nome Cliente ? " + existeCliente);
+
+            System.out.println("<--- Atualizando clientes --->");
             todosClientes.forEach(c -> {
                 c.setNome(c.getNome() + " atualizado."); //para cada nome será inserido a palavra "atualizado"
                 clientes.save(c);
             });
 
-            System.out.println("Listando clientes atualizados");
+            System.out.println("<--- Listando clientes atualizados --->");
             todosClientes = clientes.findAll();
             todosClientes.forEach(System.out::println); //imprimir todas os dados atualizados
 
-            System.out.println("Buscando clientes");
+            System.out.println("<--- Buscando clientes --->");
             clientes.findByNomeLike("Dan").forEach(System.out::println);
 
-            System.out.println("Deletando clientes");
+            System.out.println("<--- Deletando clientes --->");
             clientes.findAll().forEach(c -> {
                 clientes.delete(c); //deletar cada cliente encontrado
             });
--- src/main/java/estudos/domain/repositorio/Clientes.java
+++ src/main/java/estudos/domain/repositorio/Clientes.java
@@ -7,5 +7,14 @@ import java.util.List;
 
 public interface Clientes extends JpaRepository<Cliente, Integer> {
 
-    List<Object> findByNomeLike(String nome); //query methods
+    // select c from Cliente c where c.nome like :nome;
+    List<Cliente> findByNomeLike(String nome); //query methods
+
+    List<Cliente> findByNomeOrId(String nome, Integer id); //buscar por nome ou id
+
+    List<Cliente> findByNomeOrIdOrderById(String nome, Integer id); //buscar por nome ou id e ordernar por id, e a declaração deve ser na mesma ordem
+
+    Cliente findOneByNome(String nome); //se o nome do cliente for unico, vai retornar apenas com registro
+
+    boolean existsByNome(String nome);
 }
