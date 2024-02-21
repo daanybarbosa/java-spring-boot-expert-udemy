0e9d43a2c987442f93a2d3fcfa7143776901cd7f
Daniele Barbosa <daanybsilva@gmail.com>
2024-02-21 19:43:26 -0300

    Requisicao GET com parametros e ResponseEntity

--- README.md
+++ README.md
@@ -7,10 +7,25 @@
 - Trabalhar com o Lombok project
 - Utilizar Spring Boot DevTools
 
+## Bando de Dados - H2
+- http://localhost:8080/h2-console
+- username: sa
+- password: password
+
 ## URL
 
 #### helloCliente
 - http://localhost:8080/api/clientes/hello/{nome}
 - Exemplo: http://localhost:8080/api/clientes/hello/Fulano
 
-
+#### Exibir cliente
+- http://localhost:8080/api/clientes/{id}
+- Exemplo: http://localhost:8080/api/clientes/1
+- Retorno: 
+```
+{
+  "id": 1,
+  "nome": "Fulano",
+  "pedidos": []
+}
+```
--- src/main/java/estudos/domain/api/controller/ClienteController.java
+++ src/main/java/estudos/domain/api/controller/ClienteController.java
@@ -1,12 +1,47 @@
 package estudos.domain.api.controller;
 
 import estudos.domain.entity.Cliente;
+import estudos.domain.repository.Clientes;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @Controller //defini essa classe como um controlador REST
-@RequestMapping("/api/clientes") //defini as urls de acesso aos controllers
+//@RequestMapping("/api/clientes") //defini as urls de acesso aos controllers
 public class ClienteController {
+
+    private Clientes clientes;
+
+    public ClienteController(Clientes clientes) {
+        this.clientes = clientes;
+    }
+
+    @GetMapping("/api/clientes/{id}") //é equivalente ao @RequestMapping
+    @ResponseBody
+    public ResponseEntity getClienteById(@PathVariable Integer id){
+
+        Optional<Cliente> cliente = clientes.findById(id);
+
+        // exemplo 1
+        if (cliente.isPresent()){
+            return ResponseEntity.ok(cliente.get()); //código HTTP
+        }
+
+        // exemplo 2
+        /*if (cliente.isPresent()){
+            HttpHeaders headers = new HttpHeaders();
+            headers.put("Authorization", "token");
+            ResponseEntity<Cliente> responseEntity = new ResponseEntity<>(cliente.get(),headers, HttpStatus.OK);
+        }*/
+
+        return ResponseEntity.notFound().build();
+    }
+
+
 
     @RequestMapping(
             value = {"/hello/{nome}", "/api/hello"}, //{} variavel que será recebida por url, podendo reconhecer as duas requisições
@@ -24,7 +59,7 @@ public class ClienteController {
             method = RequestMethod.GET
     )
     @ResponseBody
-    public String helloClienteGET(@PathVariable("nome") String nomeCliente){ // @PathVariable - defini que irá receber uma variavel via url
+    public String helloClienteGET(@PathVariable("nome") String nomeCliente){ // @PathVariable - defini que irá receber uma variavel via url, entre () se a variavel estiver diferente do parametro, nesse caso, nomeCliente != nome
         return String.format("Hello %s ", nomeCliente);
     }
 }
