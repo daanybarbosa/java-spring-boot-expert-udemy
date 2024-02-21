1feb80cf616378414e998697ab190f1492e72228
Daniele Barbosa <daanybsilva@gmail.com>
2024-02-21 10:56:24 -0300

    mapeando a entidade ItensPedido

--- src/main/java/estudos/domain/entity/ItemPedido.java
+++ src/main/java/estudos/domain/entity/ItemPedido.java
@@ -3,13 +3,28 @@ package estudos.domain.entity;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
+@Entity
+@Table
 public class ItemPedido {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Integer id;
+
+    @ManyToOne //muitos ItensPedidos para um Pedido
+    @JoinColumn(name = "pedido_id")
     private Pedido pedido;
+
+    @ManyToOne //muitos ItensPedidos para um Produto
+    @JoinColumn(name = "produtos_id")
     private Produto produto;
+
+    @Column(name = "quantidade")
     private Integer quantidade;
 
 }
