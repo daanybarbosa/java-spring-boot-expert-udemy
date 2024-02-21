ab30b0273d68b36158a08eb88f5e34002ba67073
Daniele Barbosa <daanybsilva@gmail.com>
2024-02-21 18:26:39 -0300

    Mapeando as requisicoes com Request Mapping

--- src/main/java/estudos/domain/api/controller/ClienteController.java
+++ src/main/java/estudos/domain/api/controller/ClienteController.java
@@ -1,18 +1,30 @@
 package estudos.domain.api.controller;
 
+import estudos.domain.entity.Cliente;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller //defini essa classe como um controlador REST
 @RequestMapping("/api/clientes") //defini as urls de acesso aos controllers
 public class ClienteController {
 
-    @RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET) //{} variavel que será recebida por url
+    @RequestMapping(
+            value = {"/hello/{nome}", "/api/hello"}, //{} variavel que será recebida por url, podendo reconhecer as duas requisições
+            method = RequestMethod.POST,
+            consumes = {"application/json", "application/xml"}, //conteudo que essa requisição pode receber - json/xml
+            produces = {"application/json", "application/xml"} //conteudo que essa requisição vai retornar o objeto
+    )
     @ResponseBody
-    public String helloCliente(@PathVariable("nome") String nomeCliente){ // @PathVariable - defini que irá receber uma variavel via url
+    public String helloClientePOST(@PathVariable("nome") String nomeCliente, @RequestBody Cliente cliente){ // @PathVariable - defini que irá receber uma variavel via url
+        return String.format("Hello %s ", nomeCliente);
+    }
+
+    @RequestMapping(
+            value = {"/hello/{nome}", "/api/hello"}, //{} variavel que será recebida por url, podendo reconhecer as duas requisições
+            method = RequestMethod.GET
+    )
+    @ResponseBody
+    public String helloClienteGET(@PathVariable("nome") String nomeCliente){ // @PathVariable - defini que irá receber uma variavel via url
         return String.format("Hello %s ", nomeCliente);
     }
 }
